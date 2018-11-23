e2046493a69a5459021dddd03eaa1420bb84e15c
blg0018 <35704573+blg0018@users.noreply.github.com>
2018-11-23 17:38:52 -0600

    Add files via upload

--- coresets.py
+++ coresets.py
@@ -34,9 +34,31 @@ def load_kdd(filename):
         blocks[block_id] = Block(block_id)
 
       blocks[block_id].addExample(Example(block_id,example_id,protein_class,feature_values))
-      print(len(blocks[block_id].examples)) #Test print
+	  
+def create_coreset(m):
+	print("create coreset here")
+	
+def export_coreset():
+	new_line = '' #prevents a newline being printed at the beginning
+	export_file = open("export.dat", "w+")
+	print("Exporting lightweight coreset...")
+	for block in blocks:
+		for example in blocks[block].examples:
+			export_file.write(new_line+example.parent+'\t'+example.id+'\t'+example.protein_class)
+			for value in example.features:
+				export_file.write('\t'+value)
+			new_line = '\n' #every line after the first has a newline printed
+	export_file.close()
 
 blocks = {} 
 
-print(len(sys.argv))
+#parameter checking and assignment
+if len(sys.argv) != 2:
+	print("Incorrect number of parameters.")
+	print("usage: python coresets.py m")
+	exit()
+else:
+	m = sys.argv[1]
+	
 load_kdd('test_file.dat') #using subset of first 5 lines
+export_coreset()
